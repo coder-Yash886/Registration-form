0adde5c857d04ce74686b78a187feafb193bc319
yash kumar <yashkumar.967565@gmail.com>
2025-11-01 23:32:36 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,7 @@ const Header = () => {
     <header className="w-full text-center pt-16 sm:pt-12 md:pt-10 pb-4 relative">
       <img
         src={logo1}
-<<<<<<< HEAD
-        alt="Team O•S Logo"
-=======
         alt="Team OSS Logo"
->>>>>>> bda054fe89719f28196ce0a92324cf3f1753aef3
         className="absolute top-5 left-4 sm:left-6 w-10 sm:w-12 md:w-14 h-auto object-contain"
       />
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-cyan-400 tracking-wider mt-4 sm:mt-2">
