82e5b0fcefb2246287b0ee531a89817727764418
Yash Kumar <yashkumar.967565@gmail.com>
2025-11-02 13:00:16 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full text-center py-4 text-sm sm:text-base text-gray-300 mt-8">
-       {new Date().getFullYear()} 
+       
     </footer>
   );
 };
